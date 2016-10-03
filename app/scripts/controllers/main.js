7456999f19007788116fe98df0e65158ae2929c1
Brooke.Haile-Mariam <Brooke.Haile-Mariam@target.com>
2016-10-03 08:46:59 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularTestApp
+ */
+
+var app = angular.module("starbucksFinder", []);
+var MainCtrl = function($scope, $http) {
+
+  var onUserComplete = function(response) {
+    $scope.user = response.data;
+    $http.get($scope.user.repos_url)
+      .then(onRepos, onError);
+  };
+
+  var onRepos = function(response) {
+    $scope.repos = response.data;
+  };
+
+  var onError = function(reason) {
+    $scope.error = "Could not fetch the data";
+  };
+
+  $scope.search = function(username) {
+    $http.get("https://api.github.com/users/" + username)
+      .then(onUserComplete, onError);
+  };
+
+  $scope.username = "angular";
+  $scope.message = "GitHub Viewer";
+  $scope.repoSortOrder = "-stargazers_count";
+};
+
+app.controller("MainCtrl", ["$scope", "$http", MainCtrl]);
