de4ca204b063cf27141bb42a3d54d3d8a9d52ab2
Lorenzo <renzob.kc@gmail.com>
2016-10-17 07:42:42 -0500

    recraeted the app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,10 +24,10 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -2,13 +2,13 @@
 
 /**
  * @ngdoc function
- * @name photoDropApp.controller:MainCtrl
+ * @name photoDropApp.controller:AboutCtrl
  * @description
- * # MainCtrl
+ * # AboutCtrl
  * Controller of the photoDropApp
  */
 angular.module('photoDropApp')
-  .controller('MainCtrl', function () {
+  .controller('AboutCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
